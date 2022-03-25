7f2da9ed340cc1115ff67b8f7e5ed49f13c69cdd
n00bNn00b <reldewan9@gmail.com>
2022-03-26 03:01:21 +0600

    style changed for book info titles and added span tag including footer

--- src/components/BookCard/BookCard.js
+++ src/components/BookCard/BookCard.js
@@ -2,13 +2,28 @@ import React from "react";
 import "./BookCard.css";
 
 const BookCard = (props) => {
-  const { price, author_name, book_name, image } = props.bookInfo;
+  const { price, author_name, book_name, image, genre } = props.bookInfo;
   return (
     <div className="card-container">
       <img src={image} alt="" />
-      <p>Book Name: {book_name}</p>
-      <p>Author: {author_name}</p>
-      <p>Price: ${price}</p>
+      <div className="card-info">
+        <p className="title">
+          {" "}
+          <span>Book Name:</span> {book_name}{" "}
+        </p>
+        <p>
+          {" "}
+          <span className="info-title">Genre:</span> {genre}{" "}
+        </p>
+        <p>
+          {" "}
+          <span className="info-title">Author:</span> {author_name}
+        </p>
+        <p>
+          <span className="info-title">Price:</span>{" "}
+          <span className="price">${price}</span>{" "}
+        </p>
+      </div>
       <button className="btn">Add to WishList</button>
     </div>
   );
